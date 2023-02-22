c920ea027a4b25e2a6ed7031606d9c676a546ce0
Timir Ammosov <timiramm@gmail.com>
2023-02-22 14:48:45 +0700

    minor update

--- public/js/client.js
+++ public/js/client.js
@@ -10,10 +10,58 @@ const modalRegistration = document.getElementById('modalRegistration');
 const modalLogin = document.getElementById('modalLogin');
 const modalCreateMatch = document.getElementById('modalCreateMatch');
 
+const wrapperFindMatch = document.querySelector('.findMatchListner');
+
+wrapperFindMatch?.addEventListener('click', async (event) => {
+  if (event.target.dataset.findmatch === 'findMatch') {
+    event.preventDefault();
+    const sportid = Number(event.target.dataset.sportid);
+    const response = await fetch('/match/bysport', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ sport_id: sportid }),
+    });
+    const result = await response.json();
+    table.style.gridTemplateRows = `repeat(${result.length}, 1fr)`;
+    table.innerHTML = `<div style="grid-area: 1 / 1" class="gridItem">
+          Когда</div>
+       <div style="grid-area: 1 / 2" class="gridItem">Где</div>
+       <div style="grid-area: 1 / 3" class="gridItem">Условия участия</div>
+       <div style="grid-area: 1 / 4" class="gridItem">Контакты</div>
+       <div style="grid-area: 1 / 5" class="gridItem">Отозвались</div>
+  ${result.map((el, index) => (`<div style="grid-area: ${index + 2} / 1" class="table-row__data">
+         ${el.date}
+       </div>
+       <div style="grid-area: ${index + 2} / 2" class="table-row__data">
+         ${el.address}
+       </div>
+     <div style="grid-area: ${index + 2} / 3" class="table-row__data">
+       ${el.conditions}
+     </div>
+     <div style="grid-area: ${index + 2} / 4" class="table-row__data">
+       ${el.contacts}
+     </div>
+     <div style="grid-area: ${index + 2} / 5" class="table-row__data">
+       ${el.players.length}
+       /${el.max_players}
+     </div>
+`)).join('')}`;
+
+    slider.style.display = 'none';
+    table.style.display = 'grid';
+    tableHeader.style.display = 'grid';
+  }
+});
+
 ulList?.addEventListener('click', async (event) => {
   event.preventDefault();
   if (event.target.id === 'homepage') {
-    window.location.replace('/');
+    // window.location.replace('/');
+    slider.style.display = 'block';
+    table.style.display = 'none';
+    tableHeader.style.display = 'none';
   }
   if (event.target.id === 'regLink') {
     modalRegistration.style.display = 'flex';
@@ -26,6 +74,7 @@ ulList?.addEventListener('click', async (event) => {
   }
   if (event.target.id === 'logoutLink') {
     await fetch('/user/logout');
+    window.location.replace('/');
   }
   if (event.target.id === 'findMatchLink') {
     const response = await fetch('/match/bysport', {
@@ -59,7 +108,7 @@ ulList?.addEventListener('click', async (event) => {
          ${el.players.length}
          /${el.max_players}
        </div>
- `)).join('')}`
+ `)).join('')}`;
 
     slider.style.display = 'none';
     table.style.display = 'grid';
--- src/views/pages/Homepage.jsx
+++ src/views/pages/Homepage.jsx
@@ -7,20 +7,20 @@ const Registration = require('../components/Registration');
 function Homepage({ user, sports }) {
   return (
     <Layout user={user}>
-      <div className="wrapper">
-      <div className="tableGrid" id="table_header" style={{ gridTemplateColumns: `repeat(${sports.length}, 1fr)`,  display: 'none' }}>
-        {sports?.map((sport, index) => (
-          <h4 style={{ gridArea: `1 / ${index + 1}` }} className="headerSports" id={sport.id} key={sport.id}>{sport.title}</h4>
-        ))}
-      </div>
-      <div id="table" className="tableGrid" style={{ gridTemplateColumns: 'repeat(5, 1fr)', display: 'none' }} />
+      <div className="wrapper findMatchListner">
+        <div className="tableGrid" id="table_header" style={{ gridTemplateColumns: `repeat(${sports.length}, 1fr)`, display: 'none' }}>
+          {sports?.map((sport, index) => (
+            <h4 style={{ gridArea: `1 / ${index + 1}` }} className="headerSports" data-findmatch="findMatch" data-sportid={sport.id} key={sport.id}>{sport.title}</h4>
+          ))}
+        </div>
+        <div id="table" className="tableGrid" style={{ gridTemplateColumns: 'repeat(5, 1fr)', display: 'none' }} />
         <div className="cd-slider">
           <ul className="slider__list">
             <li>
               <div id="slider__image-1" className="image" />
               <div className=" content">
                 <h2>Футбол</h2>
-                <a className="slider__link" href="#">
+                <a data-sportid="1" className="slider__link" data-findmatch="findMatch" href="/">
                   Найти игру
                 </a>
               </div>
@@ -29,7 +29,7 @@ function Homepage({ user, sports }) {
               <div id="slider__image-2" className="image" />
               <div className="content">
                 <h2>Баскетбол</h2>
-                <a className="slider__link" href="#">
+                <a data-sportid="2" className="slider__link" data-findmatch="findMatch" href="/">
                   Найти игру
                 </a>
               </div>
@@ -38,7 +38,7 @@ function Homepage({ user, sports }) {
               <div id="slider__image-3" className="image" />
               <div className="content">
                 <h2>Dota 2</h2>
-                <a className="slider__link" href="#">
+                <a data-sportid="5" className="slider__link" data-findmatch="findMatch" href="/">
                   Найти игру
                 </a>
               </div>
@@ -47,7 +47,7 @@ function Homepage({ user, sports }) {
               <div id="slider__image-4" className="image" />
               <div className="content">
                 <h2>Теннис</h2>
-                <a className="slider__link" href="#">
+                <a data-sportid="3" className="slider__link" data-findmatch="findMatch" href="/">
                   Найти игру
                 </a>
               </div>
@@ -56,7 +56,7 @@ function Homepage({ user, sports }) {
               <div id="slider__image-5" className="image" />
               <div className="content">
                 <h2>Волейбол</h2>
-                <a className="slider__link" href="#">
+                <a data-sportid="4" className="slider__link" data-findmatch="findMatch" href="/">
                   Найти игру
                 </a>
               </div>
@@ -65,7 +65,7 @@ function Homepage({ user, sports }) {
               <div id="slider__image-6" className="image" />
               <div className="content">
                 <h2>Монополия</h2>
-                <a className="slider__link" href="#">
+                <a data-sportid="6" className="slider__link" data-findmatch="findMatch" href="/">
                   Найти игру
                 </a>
               </div>
