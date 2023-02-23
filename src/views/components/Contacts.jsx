a4f1d978186a5aa389e8203929b26e168600a783
Dmitry Borisenko <brdima92@gmail.com>
2023-02-23 21:56:46 +0300

    tournament fix

--- src/views/components/Contacts.jsx
+++ src/views/components/Contacts.jsx
@@ -29,6 +29,34 @@ module.exports = function Contacts({ user }) {
         </div>
         <div id="contactsMap" className="contacts__map" />
       </div>
+      <div className="container">
+        <h2 className="title contacts__title">Контакты</h2>
+        <adress className="contacts__adress">
+          <ul className="contacts__list list-reset flex">
+            <li className="contacts__item contacts__item-adress flex">
+              <strong className="contacts__item-caption">Наш адрес:</strong>
+              <p className="contacts__item-text">
+                Gang Tanjung Mekar, Jimbaran, South Kuta, Badung Regency, Bali 80361
+
+              </p>
+            </li>
+            <li className="contacts__item contacts__item-phone flex">
+              <strong className="contacts__item-caption">Телефон:</strong>
+              <div className="contacts__link-wrapper">
+                <a className="contacts__item-link" href="tel:+79876543210">+7 (987) 654-32-10</a>
+                <a className="contacts__item-link" href="tel:+71234567890">+7 (123) 456-78-90</a>
+              </div>
+            </li>
+            <li className="contacts__item contacts__item-email flex">
+              <strong className="contacts__item-caption ">Email</strong>
+              <div className="contacts__link-wrapper">
+                <a className="contacts__item-link" href="mailto:example@example.com">example@example.com</a>
+                <a className="contacts__item-link" href="mailto:support@example.com">support@example.com</a>
+              </div>
+            </li>
+          </ul>
+        </adress>
+      </div>
     </div>
   );
 };
