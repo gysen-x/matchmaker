2bafc5d637dcb8258067322307b36b9d207f2d8c
Semochka <semochkaelse@gmail.com>
2023-02-23 15:18:54 +0400

    work with stats

---
+++ db/migrations/20230223080736-create-match-stats.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('MatchStats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      match_id: {
+        type: Sequelize.INTEGER
+      },
+      stats: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('MatchStats');
+  }
+};
