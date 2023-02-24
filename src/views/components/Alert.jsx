b84237f17a03b8107e54e122b92da8032f2ced2a
Liquid1617 <ilignatow@gmail.com>
2023-02-24 18:17:06 +0800

    alldone

---
+++ src/views/components/Alert.jsx
@@ -0,0 +1,15 @@
+const React = require("react");
+//
+module.exports = function Alert() {
+  return (
+    <div className="modal_window" id="modalAlert">
+      <div className="modal_content">
+        <div className="card">
+          <div className="card-body">
+            <p />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
