c226813211a39d955ad8e1b1f11e40300139736a
Semochka <semochkaelse@gmail.com>
2023-02-24 12:06:50 +0400

    add nodemailer

--- src/routes/contactRouter.js
+++ src/routes/contactRouter.js
@@ -1,10 +1,41 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const contactController = require("../controllers/contactController");
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-router.get("/", contactController.showContacts);
-router.post("/", contactController.createSport);
+const { SECURE_EMAIL, SECURE_PASSWORD, SUPPORT_EMAIL } = process.env;
+
+const contactController = require('../controllers/contactController');
+
+router.get('/', contactController.showContacts);
+router.post('/', contactController.createSport);
+router.post('/feedback', async (req, res) => {
+  try {
+    const { email, title, text } = req.body;
+    const htmlForMessage = `<b>${title}</b><br /><b>${text}</b>`;
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.rambler.ru',
+      port: 465,
+      secure: true,
+      auth: {
+        user: SECURE_EMAIL,
+        pass: SECURE_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: SECURE_EMAIL,
+      to: SUPPORT_EMAIL,
+      subject: `FEED BACK FROM ${email}`,
+      html: `${htmlForMessage}`,
+    });
+    res.json({ message: 'Thank you for feedback' });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
